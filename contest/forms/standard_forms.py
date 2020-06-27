71b5ec51f392f3f80b595bfc34e9800247b97b4d
Anna Ehlert-Świderska <annaehlert9@gmail.com>
2020-06-27 15:07:06 +0200

    first part of views

--- contest/forms/standard_forms.py
+++ contest/forms/standard_forms.py
@@ -4,6 +4,6 @@ from django import forms
 class AuthorForm(forms.Form):
     first_name = forms.CharField(label="First name", required=False)
     last_name = forms.CharField(label="Last name", required=False)
-    birth_date = forms.DateField(label="Birth date", required=False)
+    birth_date = forms.DateField(label="Birth date", required=False, widget=forms.SelectDateWidget)
     band_name = forms.CharField(label="Band name")
-    debut = forms.DateField(label="Debut date", required=False)
+    debut = forms.DateField(label="Debut date", required=False, widget=forms.SelectDateWidget)
--- contest/views/standard_views.py
+++ contest/views/standard_views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+
+from contest.forms.standard_forms import AuthorForm
+from contest.models import Author
+
 
 class CommonIndexView(View):
     def get(self, request):
@@ -9,3 +13,80 @@ class CommonIndexView(View):
 class PanelIndexView(View):
     def get(self, request):
         return render(request, "contest/panel/index.html")
+
+
+class AuthorIndexView(View):
+    def get(self, request):
+        authors = Author.objects.all().order_by('band_name')
+        return render(request, "contest/panel/author/index.html", {
+            "authors": authors
+        })
+
+class AuthorCreateView(View):
+
+    def get(self, request):
+        return render(request,
+                      "contest/panel/author/create.html", {
+            "form": AuthorForm()}
+                      )
+
+    def post(self, request):
+        form = AuthorForm(request.POST)
+        if not form.is_valid():
+            return render(
+                request,
+                "contest/panel/author/create.html",
+                {"form": form}
+            )
+        Author.objects.create(
+            first_name=form.cleaned_data['first_name'],
+            last_name=form.cleaned_data['last_name'],
+            band_name=form.cleaned_data['band_name'],
+            birth_date=form.cleaned_data['birth_date'],
+            debut=form.cleaned_data['debut'])
+        return redirect('panel:authors:index')
+            # if Author.objects.filter(name=band_name).exists():
+            #     return render(request, "contest/panel/author/index.html", {
+            #         "form": form,
+            #         "to_be_add_2": to_be_add_2
+            #     })
+
+class AuthorDeleteView(View):
+
+    def get(self, request, id):
+        author = Author.objects.get(id=id)
+        author.delete()
+        return redirect('panel:authors:index')
+
+class AuthorUpdateView(View):
+    def get(self, request, id):
+        author = Author.objects.get(id=id)
+        form = AuthorForm({
+            "first_name": author.first_name,
+            "last_name": author.last_name,
+            "band_name": author.band_name,
+            "birth_date": author.birth_date,
+            "debut": author.debut
+        })
+        return render(request, "contest/panel/author/edit.html", {
+            "form": form,
+            "author": author
+        })
+
+    def post(self, request, id):
+        author = Author.objects.get(id=id)
+        form = AuthorForm(request.POST)
+        if not form.is_valid():
+            return render(request, "contest/panel/author/edit.html", {
+                "form": form,
+                "author": author
+            })
+
+        author.first_name = form.cleaned_data['first_name']
+        author.last_name = form.cleaned_data['last_name']
+        author.band_name = form.cleaned_data['band_name']
+        author.birth_date = form.cleaned_data['birth_date']
+        author.debut = form.cleaned_data['debut']
+        author.save()
+        return redirect('panel:authors:index')
+
